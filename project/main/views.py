32d3d2b3c93d6df1a27415b477d2834dc9137ddf
akhisyabab <akhisyabab@gmail.com>
2019-12-06 00:55:04 +0700

    product detail view

--- project/main/views.py
+++ project/main/views.py
@@ -11,4 +11,9 @@ main_blueprint = Blueprint('main', __name__, template_folder='templates')
 @main_blueprint.route('/')
 def home():
     new_products = Product.query.order_by(Product.id.desc()).limit(8).all()
-    return render_template('home.html', new_products=new_products)
+    return render_template('main.html', new_products=new_products)
+
+@main_blueprint.route('/product_detail/<product_id>/', methods=['GET', 'POST'])
+def product_detail(product_id):
+    product = Product.find_by_id(product_id)
+    return render_template('main_product_detail.html', product=product)
